d693b64a74965df643f06d09489ef450923296bf
tal bendet <tal.bendet@amdocs.com>
2023-04-28 10:27:16 +0300

    add link functionality to explore and logo btn

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Home = (): JSX.Element => {
   return (
     <main id="main" className="grid-container grid-container--home">
@@ -15,12 +17,12 @@ const Home = (): JSX.Element => {
       </div>
 
       <div>
-        <a
-          href="#"
+        <Link
+          to=".."
           className="large-button uppercase ff-serif text-dark bg-white"
         >
           Explore
-        </a>
+        </Link>
       </div>
     </main>
   );
